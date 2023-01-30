6abf0e9161eea9ecd9d2731726d0d0568afd3c86
EveOgutu <120215430+EveOgutu@users.noreply.github.com>
2023-01-30 12:40:27 +0300

    Edited script

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const main = document.getElementById('main');
 const addUserBtn = document.getElementById('add-user');
-const doubleBtn = document.getElementById('double');
-const showMillionairesBtn = document.getElementById('show-millionaires');
+const doubleBtn = document.getElementById('double-money');
+const showMillionairesBtn = document.getElementById('show-only-millionaires');
 const sortBtn = document.getElementById('sort');
 const calculateWealthBtn = document.getElementById('calculate-wealth');
 
