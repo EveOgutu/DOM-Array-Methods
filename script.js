a3c307cdbdb598abbb5f383db2154bcaea9e12cf
EveOgutu <120215430+EveOgutu@users.noreply.github.com>
2023-01-30 12:28:40 +0300

    Updated script

--- script.js
+++ script.js
@@ -30,11 +30,34 @@ async function getRandomUser() {
 //Double everyone's money
 function doubleMoney() {
     data = data.map(user => {
-        return {...user, money: user.money * 2};
+        return { ...user, money: user.money * 2};
     });
 
 
     updateDOM();
+}
+
+//sort users by richest
+function sortByRichest() {
+    data.sort((a, b) => b.money - a.money);
+
+    updateDOM();
+}
+
+//Filter only millionaires
+function showMillionaires() {
+    data = data.filter(user => user.money > 1000000);
+
+    updateDOM();
+}
+
+//Calculate the total wealth
+function calculateWealth() {
+    const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+    const wealthEl = document.createElement('div');
+    wealthEl.innerHTML= `<h3>Total Wealth: <strong>${formatMoney(wealth)}</strong></h3>`;
+    main.appendChild(wealthEl);
+
 }
 
 //Add new object to data array
@@ -69,3 +92,6 @@ function formatMoney(number) {
 //Event listeners
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
